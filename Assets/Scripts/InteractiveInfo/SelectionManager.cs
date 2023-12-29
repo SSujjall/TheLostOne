eb44bcac173355131e6c314b9fa6f4323328c787
Sujal Maharjan <sum0590@my.londonmet.ac.uk>
2023-12-29 19:15:22 +0545

    Folder structure change and Main Menu Added
    Folder hierarchy was changed and new Main Menu was added.

--- Assets/Scripts/InteractiveInfo/SelectionManager.cs
+++ Assets/Scripts/InteractiveInfo/SelectionManager.cs
@@ -1,20 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
+using JetBrains.Annotations;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class SelectionManager : MonoBehaviour
 {
- 
+
     public GameObject interaction_Info_UI;
-    Text interaction_text;
- 
+    TextMeshProUGUI interaction_text;
+    readonly float maxDistance = 7f;
+
     private void Start()
     {
-        interaction_text = interaction_Info_UI.GetComponent<Text>();
+        interaction_text = interaction_Info_UI.GetComponent<TextMeshProUGUI>();
     }
- 
+
     void Update()
     {
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
@@ -22,18 +24,27 @@ public class SelectionManager : MonoBehaviour
         if (Physics.Raycast(ray, out hit))
         {
             var selectionTransform = hit.transform;
- 
+
             if (selectionTransform.GetComponent<InteractableObject>())
             {
-                interaction_text.text = selectionTransform.GetComponent<InteractableObject>().GetItemName();
-                interaction_Info_UI.SetActive(true);
+                float distance = Vector3.Distance(Camera.main.transform.position, selectionTransform.position);
+
+                if (distance <= maxDistance)
+                {
+                    interaction_text.text = selectionTransform.GetComponent<InteractableObject>().GetItemName();
+                    interaction_Info_UI.SetActive(true);
+                }
+
+                else
+                {
+                    interaction_Info_UI.SetActive(false);
+                }
             }
-            else 
-            { 
+            else
+            {
                 interaction_Info_UI.SetActive(false);
             }
- 
+
         }
     }
 }
- 
---
+++ Assets/Scripts/Menus/MainMenu.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+    public bool isPaused;
+
+
+    public void Resume()
+    {
+        Time.timeScale = 1.0f;
+        isPaused = false;
+        SceneManager.LoadScene("GameScene");
+    }
+
+    public void exitGame()
+    {
+        Application.Quit();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.Escape))
+        {
+            if (isPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                SceneManager.LoadScene("Menu");
+                isPaused = true;
+            }
+        }
+    }
+}
